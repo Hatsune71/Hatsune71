2d00a49efacebfb8d1b66930ec206dee5bd00c4d
Hatsune-Miku <49642240+Hatsune71@users.noreply.github.com>
2020-06-27 19:00:21 +0700

    Ubuntu-Initial-Setup

---
+++ setup-ubuntu.sh
@@ -0,0 +1,9 @@
+sudo apt-get update
+sudo apt-get install openjdk-8-jdk
+sudo apt-get install bison g++-multilib git gperf libxml2-utils make zlib1g-dev:amd64 zip liblz4-tool libncurses5 libssl-dev bc flex
+mkdir ~/bin
+curl http://commondatastorage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
+chmod a+x ~/bin/repo
+sudo echo "export PATH=~/bin:$PATH" ~/.bashrc
+source ~/.bashrc
+
