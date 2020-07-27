1f15693250e6a6fd64ce63890c022c56571579b3
Hatsune-Miku <49642240+Hatsune71@users.noreply.github.com>
2020-07-27 14:25:58 +0700

    add Arch Setup Android environment
    Credits : 
    Akhil Narang <akhilnarang.1999@gmail.com>
    merothh <merothh@gmail.com>
    
    Signed-off-by: Hatsune71 <evraisecheat2@gmail.com>

---
+++ arch-setup.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# Script to setup an android build environment on Arch Linux and derivative distributions
+
+clear
+# Uncomment the multilib repo, incase it was commented out
+sudo sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
+echo "Installing Dependencies!"
+# Update
+sudo pacman -Syyu
+# Install pacaur
+sudo pacman -S base-devel git wget multilib-devel cmake svn clang lzip patchelf inetutils python2-distlib
+# Install ncurses5-compat-libs, lib32-ncurses5-compat-libs, aosp-devel, xml2, and lineageos-devel
+for package in ncurses5-compat-libs lib32-ncurses5-compat-libs aosp-devel xml2 lineageos-devel; do
+    git clone https://aur.archlinux.org/"${package}"
+    cd "${package}" || continue
+    makepkg -si --skippgpcheck
+    cd - || break
+    rm -rf "${package}"
+done
+
+echo -e "Installing platform tools & udev rules for adb!"
+sudo pacman -S android-tools android-udev
+
+echo "Installing Git,repo,and ccache!"
+sudo pacman -S git repo ccache
+
+echo "All Done :'D"
+echo "Don't forget to run these commands before building, or make sure the python in your PATH is python2 and not python3"
